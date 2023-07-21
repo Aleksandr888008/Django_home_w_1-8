ea27034ddf5079394c3cf5dc2e7b494747e002ac
Александр <learningenglish2804@gmail.com>
2023-07-21 21:18:35 +0400

    add

---
+++ catalog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from catalog.views import home, contacts
+
+urlpatterns = [
+    path('', home),
+    path('contacts', contacts)
+    ]
+
+# urlpatterns = [
+#     path('', contacts)
+#     ]
