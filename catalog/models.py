0e4d131e960ac91989bb18f39a3c0e87ad123358
Александр <learningenglish2804@gmail.com>
2023-07-28 16:11:55 +0400

    add

--- catalog/models.py
+++ catalog/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='наименование')
     description = models.TextField(verbose_name='описание')
     preview = models.ImageField(upload_to='products/', verbose_name='фото', **NULLABLE)
-    category = models.CharField(max_length=150, verbose_name='категория')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, max_length=150, verbose_name='категория')
     price = models.IntegerField(verbose_name='цена')
     date_create = models.DateTimeField(verbose_name='дата создания')
     date_change = models.DateTimeField(verbose_name='дата последнего изменения')
